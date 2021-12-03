52b2f4600a53dd0351faaf169cc9bdec4785593a
kyle dunbar <kylevdunbar@gmail.com>
2021-12-03 15:19:00 -0800

    Day 3 challenge 2

--- challenges/d3c2.js
+++ challenges/d3c2.js
@@ -2,5 +2,58 @@ import input from '../inputFiles/3.js';
 
 export default () => {
     const element = document.getElementById('d3c2');
-    element.innerHTML = `todo`;
+    const binaryLength = 12;
+    let filteredInput = [...input];
+    let oxygenRatingBinary = '';
+    let co2RatingBinary = '';
+
+    //oxygen rating
+    for (let i=0; i < binaryLength; i++) {
+        if (filteredInput.length === 1) {
+            continue;
+        }
+
+        const firstBitArray = filteredInput.map((bin) => {
+            const binaryNumberArray = bin.split('');
+            return parseInt(binaryNumberArray[i]);
+        });
+    
+        const zeros = firstBitArray.filter((bit) => bit === 0);
+        const ones = firstBitArray.filter((bit) => bit === 1);
+
+        filteredInput = filteredInput.filter((bin) => {
+            const binaryNumberArray = bin.split('');
+            return ones.length >= zeros.length ? binaryNumberArray[i] === '1' : binaryNumberArray[i] === '0';
+        });
+    }
+
+    oxygenRatingBinary = filteredInput[0];
+    filteredInput = [...input];
+
+    //co2 scrubber rating
+    for (let i=0; i < binaryLength; i++) {
+        if (filteredInput.length === 1) {
+            continue;
+        }
+
+        const firstBitArray = filteredInput.map((bin) => {
+            const binaryNumberArray = bin.split('');
+            return parseInt(binaryNumberArray[i]);
+        });
+    
+        const zeros = firstBitArray.filter((bit) => bit === 0);
+        const ones = firstBitArray.filter((bit) => bit === 1);
+
+        filteredInput = filteredInput.filter((bin) => {
+            const binaryNumberArray = bin.split('');
+            return zeros.length <= ones.length ? binaryNumberArray[i] === '0' : binaryNumberArray[i] === '1';
+        });
+    }
+
+    co2RatingBinary = filteredInput[0];
+
+    const oxygenRatingDecimal = parseInt(oxygenRatingBinary, 2);
+    const co2RatingDecimal = parseInt(co2RatingBinary, 2);
+
+    element.innerHTML = `oxygen: ${oxygenRatingDecimal} <br> co2: ${co2RatingDecimal} <br> oxygen * co2 = ${oxygenRatingDecimal * co2RatingDecimal}`;
 };

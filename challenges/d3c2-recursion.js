40fe422a7738a643435ed3e2d994efd1288a18bf
kyle dunbar <kylevdunbar@gmail.com>
2021-12-03 16:00:35 -0800

    Change d3c2 to recursive function

---
+++ challenges/d3c2-recursion.js
@@ -0,0 +1,47 @@
+import input from '../inputFiles/3.js';
+const element = document.getElementById('d3c2');
+const binaryLength = 12;
+
+const oxyPredicate = (ones, zeros, valueTocheck) => {
+    return ones.length >= zeros.length ? valueTocheck === '1' : valueTocheck === '0';
+}
+
+const co2Predicate = (ones, zeros, valueTocheck) => {
+    return zeros.length <= ones.length ? valueTocheck === '0' : valueTocheck === '1';
+}
+
+const filterInput = (filteredInput, predicate, index = 0) => {
+    let localInput = [...filteredInput];
+    let localFilteredInput = [];
+
+    const firstBitArray = localInput.map((bin) => {
+        const binaryNumberArray = bin.split('');
+        return parseInt(binaryNumberArray[index]);
+    });
+
+    const zeros = firstBitArray.filter((bit) => bit === 0);
+    const ones = firstBitArray.filter((bit) => bit === 1);
+
+    localFilteredInput = localInput.filter((bin) => {
+        const binaryNumberArray = bin.split('');
+        return predicate(ones, zeros, binaryNumberArray[index]);
+    });
+
+    if (localFilteredInput.length > 1 && index < binaryLength) {
+        
+        return filterInput(localFilteredInput, predicate, index + 1);
+    } else {
+        
+        return localFilteredInput;
+    }
+}
+
+export default () => {
+    let oxygenRatingBinary = filterInput(input, oxyPredicate);
+    let co2RatingBinary = filterInput(input, co2Predicate);
+
+    const oxygenRatingDecimal = parseInt(oxygenRatingBinary, 2);
+    const co2RatingDecimal = parseInt(co2RatingBinary, 2);
+
+    element.innerHTML = `oxygen: ${oxygenRatingDecimal} <br> co2: ${co2RatingDecimal} <br> oxygen * co2 = ${oxygenRatingDecimal * co2RatingDecimal}`;
+};
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import d1c2 from './challenges/d1c2.js';
 import d2c1 from './challenges/d2c1.js';
 import d2c2 from './challenges/d2c2.js';
 import d3c1 from './challenges/d3c1.js';
-import d3c2 from './challenges/d3c2.js';
+import d3c2 from './challenges/d3c2-recursion.js';
 
 d1c1();
 d1c2();
